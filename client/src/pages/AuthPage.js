97af7aa502ae34c2252961e414541384c30745a7
Myla <liudmyla12@yahoo.fr>
2021-07-22 15:26:37 +0200

    init commit

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import { useState,useEffect,useContext } from 'react'
+import { useHttp } from '../hooks/http.hook'
+import { useMessage } from '../hooks/message.hook'
+import { AuthContext} from '../context/AuthContext'
+
+
+export const AuthPage = () => {
+    const auth = useContext(AuthContext)
+    const message = useMessage()
+    const {loading,error,request,clearError} = useHttp()
+    const [form,setForm] = useState({
+        email:'',password: ''
+
+    })
+
+    useEffect( () => {
+        message(error)
+        clearError()
+    }, [error,message,clearError])
+
+    useEffect( ()=>{
+        window.M.updateTextFields()
+    },[])
+
+    const changeHandler = event => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
+
+const registerHandler = async () => {
+    try {
+        const data = await request('/api/auth/register', 'POST',{...form})
+        message(data.message)
+    } catch (e) {
+        
+    }
+}
+
+const loginHandler = async () => {
+    try {
+        const data = await request('/api/auth/login', 'POST',{...form})
+        auth.login(data.token,data.userId)
+    } catch (e) {
+        
+    }
+}
+
+    return (
+<div className="row">
+    <div className="col s6 offset-s3">
+                <h1>Make link shorter</h1>
+        <div className="card blue darken-1">
+            <div className="card-content white-text">
+                <span className="card-title">Identification</span>
+              
+                <div className="input-field">
+                        <input
+                            placeholder="Your email"
+                            id="email"
+                            type="text"
+                            name="email"
+                            value={form.email}
+                            className="yellow-input"
+                            onChange={changeHandler}
+                        />
+                <label htmlFor="email">Email</label>
+                </div>  
+                <div className="input-field">
+                        <input
+                            placeholder="Your password"
+                            id="password"
+                            type="password"
+                            name="password"
+                            value={form.password}
+                            className="yellow-input"
+                            onChange={changeHandler}
+                        />
+                <label htmlFor="password">Password</label>
+                </div>     
+            
+             
+              
+                <div className="card-action">
+                        <button
+                        className="btn yellow darken-4" 
+                        style={{marginRight: 10}}
+                        onClick={loginHandler}
+                        disabled={loading}
+                        >Login</button>
+                        <button 
+                        className="btn grey lighten-1 black-text"
+                        onClick={registerHandler}
+                        disabled={loading}
+                        >Sign In</button>
+                </div>
+                </div>  
+      </div>
+    </div>
+</div>
+    )
+}
